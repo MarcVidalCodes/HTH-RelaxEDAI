29e46a39ab7e75b7c4ad513012a614042b1e6c26
Benjamin Gavieres <benjamin.gavieres@gmail.com>
2024-09-28 10:36:06 -0400

    add i2c properties

---
+++ main/get_pulse.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+#define ADDR                                                        0x57
+#define PORT                                                        0x00
+#define SDA_GPIO                                                    GPIO_NUM_21
+#define SCL_GPIO                                                    GPIO_NUM_22
+#define I2C_FREQ_HZ                                                 100000
